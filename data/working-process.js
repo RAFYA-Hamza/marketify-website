f4ee80a16adc41fa7151bfc76e5a2bf46c60a6b4
RYham <rafyahamza99@gmail.com>
2024-09-17 20:58:10 +0100

    Some changes

---
+++ data/working-process.js
@@ -0,0 +1,38 @@
+export const working = [
+  {
+    id: "01",
+    title: "Consultation",
+    content:
+      "During the initial consultation, we will discuss your business goals and objectives, target marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+  },
+  {
+    id: "02",
+    title: "Research and Strategy Development",
+    content:
+      "In this phase, we conduct in-depth research on your industry, competitors, and target audience. Based on the findings, we develop a tailored digital marketing strategy designed to meet your business goals effectively.",
+  },
+  {
+    id: "03",
+    title: "Implementation",
+    content:
+      "With a clear strategy in place, we move to the implementation phase. This involves executing the marketing campaigns, creating content, optimizing your website, and setting up advertising efforts.",
+  },
+  {
+    id: "04",
+    title: "Monitoring and Optimization",
+    content:
+      "We continuously track the performance of your marketing efforts using advanced analytics tools. This allows us to assess campaign effectiveness and make necessary adjustments to improve results.",
+  },
+  {
+    id: "05",
+    title: "Reporting and Communication",
+    content:
+      "We believe in transparency and provide regular reports on your campaign's performance. These reports will include key metrics, insights, and recommendations to keep you informed about the progress.",
+  },
+  {
+    id: "06",
+    title: "Continual Improvement",
+    content:
+      "Digital marketing is an ongoing process. We constantly refine and adjust strategies to ensure your business continues to grow. This continuous improvement approach allows us to adapt to changing market conditions and evolving business needs.",
+  },
+];
--- script/script.js
+++ script/script.js
@@ -1,10 +1,46 @@
-function toggleCard(cards) {
-  console.log(cards);
-  const divider = document.querySelector(".process__divider");
-  const content = document.querySelector(".process__content");
-  const card = document.querySelector(".process__card");
+import { working } from "../data/working-process.js";
 
-  divider.classList.toggle("open");
-  content.classList.toggle("open");
-  card.classList.toggle("open");
-}
+let working__processHTML = "";
+
+working.forEach((work) => {
+  working__processHTML += `
+            <div class="process__card js-process__card" data-card="${work.id}">
+                <div class="process__heading">
+                    <div class="process__title">
+                        <h1 class="bold">${work.id}</h1>
+                        <h3>${work.title}</h3>
+                    </div>
+                    <div class="process__button js-process__button">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <span class="process__divider js-process__divider"></span>
+                <div class="process__content js-process__content">
+                    <p>${work.content}</p>
+                </div>
+            </div>
+ `;
+});
+
+document.querySelector(".js-process__container").innerHTML =
+  working__processHTML;
+
+document.querySelectorAll(".js-process__card").forEach((button) => {
+  button.addEventListener("click", () => {
+    const cardId = button.getAttribute("data-card");
+
+    const card = document.querySelector(
+      `.js-process__card[data-card="${cardId}"]`
+    );
+
+    const divider = card.querySelector(".js-process__divider");
+    const content = card.querySelector(".js-process__content");
+    const buttonIcon = card.querySelector(".js-process__button");
+
+    divider.classList.toggle("open");
+    content.classList.toggle("open");
+    buttonIcon.classList.toggle("open");
+    card.classList.toggle("open");
+  });
+});

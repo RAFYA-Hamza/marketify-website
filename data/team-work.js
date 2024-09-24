aa685a017e24cbbda6f573c2cc2994824a205403
RYham <rafyahamza99@gmail.com>
2024-09-24 21:30:08 +0100

    Some changes

---
+++ data/team-work.js
@@ -0,0 +1,8 @@
+export const team = [
+  {
+    id: "01",
+    title: "Consultation",
+    content:
+      "During the initial consultation, we will discuss your business goals and objectives, target marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+  },
+];

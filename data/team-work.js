e8c8fdae80c8cddc34d8515b18e71cb92856d5ae
RYham <rafyahamza99@gmail.com>
2024-09-24 21:35:43 +0100

    Some changes

--- data/team-work.js
+++ data/team-work.js
@@ -1,8 +1,50 @@
 export const team = [
   {
     id: "01",
-    title: "Consultation",
-    content:
-      "During the initial consultation, we will discuss your business goals and objectives, target marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    image: "",
+    name: "John Smith",
+    status: "CEO and Founder",
+    description:
+      "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+  },
+  {
+    id: "02",
+    image: "",
+    name: "Jane Doe",
+    status: "Director of Operations",
+    description:
+      "7+ years of experience in project management and team leadership. Strong organizational and communication skills",
+  },
+  {
+    id: "03",
+    image: "",
+    name: "Michael Brown",
+    status: "Senior SEO Specialist",
+    description:
+      "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
+  },
+  {
+    id: "04",
+    image: "",
+    name: "Emily Johnson",
+    status: "PPC Manager",
+    description:
+      "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
+  },
+  {
+    id: "05",
+    image: "",
+    name: "Brian Williams",
+    status: "Social Media Specialist",
+    description:
+      "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement",
+  },
+  {
+    id: "06",
+    image: "",
+    name: "Sarah Kim",
+    status: "Content Creator",
+    description:
+      "2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries",
   },
 ];

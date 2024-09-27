8ccdb207705cf6714e3198e03d8c8f516e9f5f1d
RYham <rafyahamza99@gmail.com>
2024-09-27 23:49:29 +0100

    Some changes

---
+++ data/testimonials.js
@@ -0,0 +1,44 @@
+export const testimonials = [
+  {
+    id: "0",
+    name: "John Smith",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+  {
+    id: "1",
+    name: "John Smith 1",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+  {
+    id: "2",
+    name: "John Smith 2",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+  {
+    id: "3",
+    name: "John Smith 3",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+  {
+    id: "4",
+    name: "John Smith 4",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+  {
+    id: "5",
+    name: "John Smith 5",
+    status: "Marketing Director at XYZ Corp",
+    Comment:
+      "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+  },
+];
